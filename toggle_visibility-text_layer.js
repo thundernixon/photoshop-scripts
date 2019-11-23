195b61876cde78d88e27bd8e9b8eb178e608ae3e
Stephen Nixon <stephen@thundernixon.com>
2019-11-23 14:23:17 -0500

    make script to toggle text layer visibility

--- toggle_visibility-text_layer.js
+++ toggle_visibility-text_layer.js
@@ -1,1 +1,31 @@
-// script to show/hide text layers in photoshop file
+// script to toggle visibility of text layers in photoshop file
+// 
+// USAGE:
+// 1. Open a document in PhotoShop
+// 2. File > Scripts > Browse... 
+// 3. Find this script and open it
+
+var debug = false
+
+var layers = app.activeDocument.artLayers
+
+for (var layer = 0; layer < layers.length; layer++) {
+
+    if (debug == true) {
+        $.writeln("------------------------------")
+        $.writeln(layers[layer].name)
+        $.writeln(layers[layer].kind)
+        $.writeln(layers[layer].kind == LayerKind.TEXT)
+        $.writeln(layers[layer].visible)
+    }
+    if (layers[layer].kind == LayerKind.TEXT) {
+        if (layers[layer].visible == true) {
+            layers[layer].visible = false
+        } else {
+            layers[layer].visible = true
+        }
+        if (debug == true) {
+            $.writeln(layers[layer].name + " is now visibility: " + layers[layer].visible)
+        }
+    }
+}
